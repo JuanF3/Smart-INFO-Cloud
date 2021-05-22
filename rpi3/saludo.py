db6b619c839933e118da80e9d47566845e85935d
Felipe Mantilla <60745140+JuanF3@users.noreply.github.com>
2021-05-22 10:05:13 -0500

    Update saludo.py

--- rpi3/saludo.py
+++ rpi3/saludo.py
@@ -6,9 +6,8 @@
 # Comentario de Felipe
 # Comentario de Daniel desde Visual
 # Comentario de Felipe 2
-<<<<<<< HEAD
 
 print(' pretecor ')
-=======
+
 # Comentario con posible conflicto - Daniel
->>>>>>> da8cbe92ece8c5d4cc824e46b6e900fbde162790
+
